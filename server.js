daf953e61f5b82c3cf8334761b17f5f4277da2c6
efrageek <efraingpj@gmail.com>
2020-05-18 11:20:33 -0300

    receiving and sending data with http

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const router = express.Router();
+
 
 const app = express();
-app.use('/', (req, res) => {
-    res.send('Hola');
-});
+
+app.use(bodyParser.json());
+app.use(router);
+
+router.get('/message', (req, res) => {
+    
+    res.send('Lista de mensajes');
+})
+
+router.post('/message', (req, res) => {
+    console.log(req.query);
+    console.log(req.body);
+    res.send('Mensaje anadido');
+})
+
+// app.use('/', (req, res) => {
+//     res.send('Hola');
+// });
 
 app.listen(3000);
 console.log('escuchando en puerto 3000');
